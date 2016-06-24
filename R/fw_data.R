b7d11298458500b99221755adf8041d504fdb21a
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2016-06-24 09:14:16 +0200

    including the permissions and access functions

---
+++ R/fw_data.R
@@ -0,0 +1,72 @@
+## data accessing functions
+
+
+
+## Core data:
+fw_info <- function(path) {
+  datastorr::github_release_info("SrivastavaLab/cesabfunctionalwebsdata",
+                                 filename=NULL,
+                                 read=readRDS,
+                                 private = FALSE,
+                                 path=NULL)
+}
+
+##' @export
+##' @rdname mydata
+##'
+##' @param local Logical indicating if local or github versions should
+##'   be polled.  With any luck, \code{local=FALSE} is a superset of
+##'   \code{local=TRUE}.  For \code{fw_version_current}, if
+##'   \code{TRUE}, but there are no local versions, then we do check
+##'   for the most recent github version.
+##'
+fw_versions <- function(local=TRUE, path=NULL) {
+  datastorr::github_release_versions(fw_info(path), local)
+}
+
+##' @export
+##' @rdname mydata
+fw_version_current <- function(local=TRUE, path=NULL) {
+  datastorr::github_release_version_current(fw_info(path), local)
+}
+
+
+
+##' @title Download CESAB Functionalwebs Data
+##'
+##' @param version Version number.  The default will load the most
+##'   recent version on your computer or the most recent version known
+##'   to the package if you have never downloaded the data before.
+##'   With \code{fw_del}, specifying \code{version=NULL} will
+##'   delete \emph{all} data sets.
+##'
+##' @param path Path to store the data at.  If not given,
+##'   \code{datastorr} will use \code{rappdirs} to find the best place
+##'   to put persistent application data on your system.  You can
+##'   delete the persistent data at any time by running
+##'   \code{fw_del(NULL)} (or \code{fw_del(NULL, path)} if you
+##'   use a different path).
+##'
+##' @export
+fw_data <-  function(version=NULL, path=NULL) {
+  datastorr::github_release_get(fw_info(path), version)
+}
+
+### authentication --------------
+
+fw_auth <- function(){
+  id <- system.file("identification.rds", package = "fwdata")
+
+  id_raw <- readRDS(id)
+
+  password <- getPass::getPass()
+
+  ## decrypt
+  k <- charToRaw(paste0(password, "1234"))
+  aes <- digest::AES(k, mode = "ECB")
+  p_w_ending <- aes$decrypt(id_raw)
+
+  answer <- substr(p_w_ending, 1, 40)
+
+  Sys.setenv(GITHUB_TOKEN=answer)
+}
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # fwdata
 small, lightweight package for accessing BWG data for the CESAB FUNCTIONALWEBS projects
+
+How to use it
+
+```r
+fw_auth()
+
+ourdata <- datastorr::github_release_info("SrivastavaLab/cesabfunctionalwebsdata",
+                               read=readRDS)
+
+datastorr::github_release_version_current(ourdata, local = FALSE)
+
+d <- datastorr::github_release_get(ourdata, "0.0.1")
+
+```
